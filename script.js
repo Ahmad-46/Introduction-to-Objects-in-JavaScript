f96c49ebc845a8ea9d110089c483dd826ee0a384
Ahmad-46 <mubbaranauman@gmail.com>
2025-02-06 07:45:11 -0700

    ler

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
 let person = {
   firstName: "Ahmad",
   age: 14,
+  hobby: "badminton",
 };
 // Display some data from the object:
 document.getElementById("demo").innerHTML =
-  person.firstName + " is " + person["age"] + "year old";
+  person.firstName + " is " + person["age"] + " year old. he love coding.his hobby is " + person.hobby;
+  //object with objects that contain value to find
+const myObject = {
+  obj1: {value: 254},
+  obj2: {value: 21},
+  obj3: {value: 525},
+  obj4: {value: 10}
+};
 
-function findMax() {
-  let max = 0;
-  for (i = 0; i < arguments.length; i++) {
-    if (arguments[i] > max) {
-        max = arguments[i];
-    }
-  }
-  return max;
-}
-document.getElementById("result").innerHTML = findMax(491,122,322,153,243);
+// Variables to hold max value 
+let maxValue = 0;
+
+//converting the object to an array
+const values = Object.values(myObject);
+
+//Using the .map() method to loop the array, find maximum value
+values.map((el) => {
+  //getting the value from each object and 
+  //comparing to existing value
+  const valueFromObject = el.value;
+  maxValue = Math.max(maxValue, valueFromObject);
+});
+
+//Printing out the result
+console.log(maxValue); 
+//Output: 525 
